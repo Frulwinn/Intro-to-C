103f1cd0e783b5f60757e301a18c5959f8a4a4fc
Frulwinn <frulwinn@gmail.com>
2019-07-16 15:04:38 -0500

    fixed fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -15,13 +15,15 @@ int fizzbuzz(int n)
     
     for (int i = 1; i <= 100; i++) 
     {
-        if (i % 3 == 0)
-            printf("%d Fizz", i);
-        if (i % 5 == 0)
-            printf("%d Buzz", i);
-        if ((i % 3 != 0) && (i % 5 != 0))
-            printf("%d FizzBuzz", i);
-        printf("\n");
+        if ((i % 15 == 0))
+            printf("%d FizzBuzz\n", i);
+        else if (i % 3 == 0)
+            printf("%d Fizz\n", i);
+        else if (i % 5 == 0)
+            printf("%d Buzz\n", i);
+        else 
+            printf("%d\n", i);
+        
     }
 
     return 0;
